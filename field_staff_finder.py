75c65334c3cee84854b9bc0b7cf67e973c3de967
Steph Herbers <herberss@carleton.edu>
2020-07-28 12:36:29 -0500

    refactored to 2 files and included pickling to prevent from repeating processing data too often and crreated find_field_locations that processes email array and then exports to csv file

--- field_staff_finder.py
+++ field_staff_finder.py
@@ -1,29 +1,36 @@
-import re
+import pickle
+import pandas as pd
 
+#return dict of lat and long based on city name
+def find_lat_and_long(location_data, city_name):
+    return location_data[city_name.lower()]
 
-# processes location txt file and outputs location_dict : {"city_name": {"lat":  "long": }}
-def process_location_data(file_name):
-    location_dict = {}
-    lat_and_long_dict = {}
-    file = open(file_name, "r")
-    for line in file:
-        city_names = set(re.findall(r',([a-zA-Z ]+)', line))
-        lat_and_long = re.findall(r'[0-9]+\.[0-9]*', line)
-        try:
-            lat_and_long_dict["lat"] = lat_and_long[0]
-            lat_and_long_dict["long"] = lat_and_long[1]
-        except:
-            pass #not proper lat or long found for this line
-        for city in city_names:
-            location_dict[city] = lat_and_long_dict
-    return location_dict
+def find_field_locations(location_data, email_array):
+    field_locations = {'Email': [], 'Time': [], 'Location': [], 'Latitude': [], 'Longitude': []}
 
-
+    for email in email_array:
+        field_locations["Email"].append(email[0])
+        field_locations["Time"].append(email[1])
+        field_locations["Location"].append(email[2])
+        lat_and_long = find_lat_and_long(location_data, email[2])
+        field_locations["Latitude"].append(lat_and_long["lat"])
+        field_locations["Longitude"].append(lat_and_long["long"])
+    return field_locations
 
 
 
 def main():
-    location_data = process_location_data("cities15000.txt")
+    processed_email_array = [["nick@dimagi.com", "2011-05-19 14:05",  "Dodoma"],
+ ["alex@dimagi.com", "2011-05-22 16:22",  "Lusaka"], ["alice@dimagi.com", "2019-12-21 17:05",  "Sydney"]] #assumption: import this from separate function
+
+    location_data =  pickle.load( open("processed_location_data.p", "rb"))
+    field_locations = find_field_locations(location_data, processed_email_array)
+
+
+
+
+    df = pd.DataFrame(field_locations, columns= ['Email', 'Time', 'Location', 'Latitude', 'Longitude'])
+    df.to_csv (r'field_staff_locations.csv', index = False, header=True)
 
 
 
